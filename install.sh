e4084e06fb8ac4963daae8f630e079806594a583
Lilanga Gamage <lilanga@live.com>
2023-08-24 20:31:14 +1000

    update script to fix some issues

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$USER" ]; then
-    USER=$(id -un)
+  USER=$(id -un)
 fi
 
 echo >&2 "====================================================================="
@@ -23,22 +23,25 @@ sudo apt-get install -y fish
 sudo chsh -s /usr/bin/fish $USER
 
 # Install tree-sitter-cli
-cargo install tree-sitter-cli
+npm i tree-sitter-cli
 
 # Install fzf
-FZF_VERSION=0.30.0
+FZF_VERSION=0.42.0
+mkdir -p ~/bin
 curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC $HOME/bin
 
 # Install nerdfont
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+cd ~/ || return
 
 # Install ripgrep
-$ curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-$ sudo dpkg -i ripgrep_13.0.0_amd64.deb
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
 
 # Install bottom
-cargo install bottom --locked
+curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.4/bottom_0.9.4_amd64.deb
+sudo dpkg -i bottom_0.9.4_amd64.deb
 
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
@@ -48,9 +51,13 @@ sudo install lazygit /usr/local/bin
 
 # Install neovim
 NVIM_VERSION=0.9.1
-sudo apt-get install -y libfuse2
+sudo apt-get update -y
+sudo apt-get install -y fuse libfuse2
 curl -L -o $HOME/bin/nvim https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
 chmod a+x $HOME/bin/nvim
 
 # Install astrovim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+
+export PATH="$HOME/bin:$PATH"
+source ~/.bashrc
